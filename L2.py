17b5d3be040f1b45c1b7b274aa371fb0ba22925a
Jiawei Wang <wangjiawei_med@hotmail.com>
2022-09-17 00:01:47 -0700

    09-17-2022

--- L2.py
+++ L2.py
@@ -3,6 +3,10 @@
 2. batch and stochastic gradient descent
 3. normal equations
 
+
 process: training data -> learning algorithm -> hypothesis
 question: how to present the hypothesis
-"""
+
+Gradient descent/Local optimum/learning rate   
+Bash Gradient Descent/stochastic gradient descent: SGD allows to train faster on large data set but produces noises and never stops at global optimum
+ """
